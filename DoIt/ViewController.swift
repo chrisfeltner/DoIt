dc7335030ebf316dcd5d04d4c032eaff01d907f9
Christopher Feltner <chris.feltner@knights.ucf.edu>
2017-02-26 00:41:48 -0500

    made tableview

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var taskList: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        taskList.dataSource = self
+        taskList.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hello"
+        return cell
+    }
 
 }
 
